f551522c60515585a1077b552e8f68fc21d05b1c
tkyg <80114433+tkyg@users.noreply.github.com>
2023-01-27 11:03:49 -0500

    commit tree ef4ff50aa92071e0f6af7073dd872e2fd68af977

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
-export function manageFriends(state, action) {
+const initialState = { friends: [] };
+
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type) {
+      case "friends/add":
+        return { friends: [...state.friends, action.payload]}
+      case 'friends/remove':
+        return { friends: state.friends.filter(friend => friend.id !== action.payload)}
+      default:
+        return state
+  }
 }
